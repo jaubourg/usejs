7a89249956b9e53257471c599b5808bef42dcf01
jaubourg <j@ubourg.net>
2014-01-17 17:33:00 +0100

    Better integration of npm install

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ var fs = require( "fs" );
 
 process.chdir( __dirname );
 
+// Let's install what's needed for testing
+exec( "bower install qunit#1.13.0", function( error ) {
+	if ( error ) {
+		console.error( "You need to install bower in order to unit test!" );
+	}
+	console.log( "qunit is installed" );
+} );
+
+// Utilities
 function read( file ) {
 	console.log( "Reading " + file + "..." );
 	return fs.readFileSync( file ) + ""
@@ -11,6 +20,26 @@ function read( file ) {
 function write( file, content ) {
 	fs.writeFileSync( file, content );
 	console.log( file + " done!" );
+}
+
+function use( names, callback ) {
+	names = names.split( " " );
+	function done() {
+		callback.apply( null, names.map( function( name ) {
+			console.log( name.split( "@" )[ 0 ] );
+			return require( name.split( "@" )[ 0 ] );
+		} ) );
+	}
+	try {
+		return done();
+	} catch ( e ) {}
+	console.log( "Installing " + names.join( " & " ) + "..." );
+	exec( "npm install " + names.join( " " ), function( error ) {
+		if ( error ) {
+			throw error;
+		}
+		done();
+	} );
 }
 
 var build = require( "./build/build" )( read );
@@ -30,23 +59,10 @@ var config = JSON.parse( read( "build/config.json" ) );
 // Generate the full text script
 var fullText = build( "build/template.js", config.version, config.modules, "src/%%.js" );
 
-// Let's install what's needed for testing
-exec( "bower install qunit#1.13.0", function( error ) {
-	if ( error ) {
-		console.error( "You need to install bower in order to unit test!" );
-	}
-} );
-
 // Let's install some packages
-console.log( "Installing packages..." );
-exec( "npm install uglify-js@2.4.9 jshint@2.4.1", function( error ) {
-	if ( error ) {
-		throw error;
-	}
-	var minified = build( "build/template.min.js", config.version, require( "uglify-js" ).minify( fullText, {
-		fromString: true
-	} ).code );
+use( "uglify-js@2.4.9 jshint@2.4.1", function( uglify, jshint ) {
 	write( "dist/use.js", fullText );
-	write( "dist/use.min.js", minified );
+	write( "dist/use.min.js", build( "build/template.min.js", config.version, uglify.minify( fullText, {
+		fromString: true
+	} ).code ) );
 } );
-
