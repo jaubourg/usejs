4789d000c93b07d3d2f3f71787c090ab06de3840
jaubourg <j@ubourg.net>
2014-01-17 02:29:57 +0100

    No more PHP

--- test/init.js
+++ test/init.js
@@ -1,1 +1,24 @@
+( function() {
+
+	function getJSON( url ) {
+		var xhr = new XMLHttpRequest();
+		xhr.open( "GET", url, false );
+		xhr.send();
+		return ( new Function( "return " + xhr.responseText + ";" ) )();
+	}
+
+	function loadScript( url ) {
+		document.write( "<script src='" + url +"'></script>\n" );
+	}
+
+	loadScript( "../dist/use.js" );
+
+	loadScript( "qunit/qunit/qunit.js" );
+
+	for( var unit in getJSON( "units.json" ) ) {
+		loadScript( "unit/" + unit + ".js" );
+	}
+
+} )();
+
 var testCount = 0;
